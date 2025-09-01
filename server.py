489e0454aeea1816b3f24b61df4089affbd0096d
jetntk-1031 <150217499+jetntk-1031@users.noreply.github.com>
2025-09-01 14:12:47 +0800

    ok

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 
 HOST = "0.0.0.0"
-PORT = 5000  # Railway will assign this dynamically
+PORT = 51731  # Railway will assign this dynamically
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
