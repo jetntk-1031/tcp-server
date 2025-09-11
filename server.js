0d1b4a8266ee0f8dec517beff59a88e8519ea394
jetntk-1031 <150217499+jetntk-1031@users.noreply.github.com>
2025-09-11 14:11:38 +0800

    update random range index2

--- server.js
+++ server.js
@@ -334,7 +334,7 @@ let getRandomRange = (min,max) => {
 
 let chooseOrderIdxByPriority = (MagObjLen,WOLen,Prob_MagLenLargerOne) => { 
     // Prob_MagLenLargerOne : probability set to always give other WO for the second Magazine
-    if(MagObjLen == 1) return 1;
+    if(MagObjLen < 1) return 1;
    
     if(WOLen == 1) return 1;
   
